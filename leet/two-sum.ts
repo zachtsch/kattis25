d7d9b6e7a24f1a4df257ceb995472f7dfd6878e7
zachtsch <zachary.tschetter@usd.edu>
2025-01-29 11:19:04 -0600

    finished two-sum

---
+++ leet/two-sum.ts
@@ -0,0 +1,14 @@
+function twoSum(nums: number[], target: number): number[] {
+    const dict : Record<number,number> = {}
+    for(let i = 0; i < nums.length; i++){
+        dict[target-nums[i]]=i;
+    }
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] in dict && i!=dict[nums[i]]){
+            return [i,dict[nums[i]]]
+        }
+    }
+    return [-1,-1]
+};
+
+console.log('test',twoSum([2,7,11,15],9))
